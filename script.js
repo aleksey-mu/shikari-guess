ba241eaf4c495978b3345d85b00ac9a096f8be4e
aleksey-mu <pacuklex@mail.ru>
2020-04-24 00:45:36 +0300

    add: add songs in base

--- script.js
+++ script.js
@@ -1,161 +1,110 @@
 const apiURL = 'https://api.lyrics.ovh';
 const TEXTCONTAINER = document.querySelector('.text-container');
+// import { tracks } from "songBase.js"
+// const { tracks } = require('songBase');
 
-const tracks = [`Acid Nation`,
-
-`Frozen Landscape`,
-
-`Adieu`,
-`Airfield`, 
-`All Eyes on the Saint`, 
-`Anaesthetist`,
-`An Ode to Lost Jigsaw Pieces (In Two Movements)`,
- `Antwerpen`,
-  `Anything Can Happen in the Next Half Hour…`, `​apøcaholics anonymøus (main theme in B minor)
-`, 
-`Arguing with Thermometers
-`,
- `Common Dreads
-`,
- `Constellations
-`,
- `Crossing The Rubicon
-`, 
-`Dear Future Historians…
-`, 
-`Destabilise
-`,
- `Elegy For Extinction
-`,
-`Enter Shikari`,
-`Empty
-`, 
-`Fanfare for the Conscious Man
-`,
- `Gandhi Mate, Gandhi
-`,
- `Gap in the Fence
-`,
- `Havoc A
-`,
- `Havoc B
-`,
- `Hectic
-`,
- `Hello Tyrannosaurus, Meet Tyrannicide
-`, 
-`Hoodwinker
-`,
- `Interlude
-`, 
-`Jonny Sniper
-`,
- `Juggernauts
-`, 
-`Keep It on Ice
-`,
- `Kickin’ Back on the Surface of Your Cheek
-`,
- `Know Your Enemy
-`,
- `Labyrinth
-`,
- `Live Outside
-`,
- `Marionettes (II. The Ascent)
-`, 
-`Marionettes (I. The Discovery Of Strings)
-`, 
-`…Meltdown
-`, 
-`​modern living....
-`, 
-`Mothership
-`, `Myopia
-`, `Never Let Go of the Microscope
-`, `Nodding Acquaintance
-`, `No Sleep Tonight
-`, `No Sssweat
-`, `OK, Time for Plan B
-`, `Pack of Thieves
-`, `Quelle Surprise
-`, `Rabble Rouser
-`, `Radiate
-`, `Rat Race
-`, `Redshift
-`, `Return to Energiser
-`, `​satellites* *
-`, `Score 22
-`, `Search Party
-`, `Shinrin-yoku
-`, `Slipshod
-`, `Solidarity
-`, `Sorry, You’re Not a Winner
-`, `Sssnakepit
-`, `Stalemate
-`, `Stand Your Ground; This Is Ancient Land
-`, `Step Up
-`, `Stop the Clocks
-`, `Supercharge
-`, `System…
-`, `Take My Country Back
-`, `The Appeal & the Mindsweep I
-`, `The Appeal & the Mindsweep II
-`, `The Bank of England`,
-, `The Bearer of Bad News`,
-
-
-`{ The Dreamer’s Hotel }
-`, 
-`The Feast
-`, 
-`THE GREAT UNKNOWN
-`, 
-`The Jester
-`, 
-`​thē kĭñg
-`, 
-`The Last Garrison
-`, 
-`The One True Colour
-`, 
-`The Paddington Frisk
-`, 
-`​the pressure’s on.
-`, 
-`There’s a Price on Your Head
-`, 
-`The Revolt of the Atoms
-`, 
-`The Sights
-`, 
-`The Spark
-`, 
-`Thumper
-`, 
-`T.I.N.A.
-`, 
-`Today Won’t Go Down in History
-`, 
-`Torn Apart
-`, 
-`Tribalism
-`, 
-`Undercover Agents
-`, 
-`Wall
-`, 
-`Waltzing Off The Face Of The Earth (I. Crescendo)
-`, 
-`Waltzing off the Face of the Earth (II. Piangevole)
-`, 
-`Warm Smiles Do Not Make You Welcome Here
-`, 
-`We Can Breathe In Space
-`, 
-`When a Jealous Man Finds a Gun
-`, 
-`Zzzonked`]
+const tracks = [
+  `Acid Nation`,
+  `Adieu`,
+  `Airfield`,
+  `All Eyes on the Saint`,
+  `Anaesthetist`,
+  `An Ode to Lost Jigsaw Pieces (In Two Movements)`,
+  `Antwerpen`,
+  `Anything Can Happen in the Next Half Hour…`,
+  `apøcaholics anonymøus (main theme in B minor)`,
+  `Arguing with Thermometers`,
+  `Common Dreads`,
+  `Constellations`,
+  `Crossing The Rubicon`,
+  `Dear Future Historians…`,
+  `Destabilise`,
+  `Elegy For Extinction`,
+  `Enter Shikari`,
+  `Empty`,
+  `Fanfare for the Conscious Man`,
+  `Fixed Eyes`,
+  `Frozen Landscape`,
+  `Gandhi Mate, Gandhi`,
+  `Gap in the Fence`,
+  `Havoc A`,
+  `Havoc B`,
+  `Hectic`,
+  `Hello Tyrannosaurus, Meet Tyrannicide`,
+  `Hoodwinker`,
+  `Insomnia`,
+  `Interlude`,
+  `Jonny Sniper`,
+  `Juggernauts`,
+  `Keep It on Ice`,
+  `Kickin’ Back on the Surface of Your Cheek`,
+  `Know Your Enemy`,
+  `Labyrinth`,
+  `Live Outside`,
+  `Marionettes (II. The Ascent)`,
+  `Marionettes (I. The Discovery Of Strings)`,
+  `…Meltdown`,
+  `modern living....`,
+  `Mothership`,
+  `Myopia`,
+  `Never Let Go of the Microscope`,
+  `Nodding Acquaintance`,
+  `No Sleep Tonight`,
+  `No Sssweat`,
+  `OK, Time for Plan B`,
+  `Pack of Thieves`,
+  `Quelle Surprise`,
+  `Rabble Rouser`,
+  `Radiate`,
+  `Rat Race`,
+  `Redshift`,
+  `Return to Energiser`,
+  `satellites* *`,
+  `Score 22`,
+  `Search Party`,
+  `Shinrin-yoku`,
+  `Slipshod`,
+  `Solidarity`,
+  `Sorry, You’re Not a Winner`,
+  `Sssnakepit`,
+  `Stalemate`,
+  `Stand Your Ground; This Is Ancient Land`,
+  `Step Up`,
+  `Stop the Clocks`,
+  `Supercharge`,
+  `System…`,
+  `Take My Country Back`,
+  `The Appeal & the Mindsweep I`,
+  `The Appeal & the Mindsweep II`,
+  `The Bank of England`,
+  `The Bearer of Bad News`,
+  `{ The Dreamer’s Hotel }`,
+  `The Feast`,
+  `THE GREAT UNKNOWN`,
+  `The Jester`,
+  `thē kĭñg`,
+  `The Last Garrison`,
+  `The One True Colour`,
+  `The Paddington Frisk`,
+  `the pressure’s on.`,
+  `There’s a Price on Your Head`,
+  `The Revolt of the Atoms`,
+  `The Sights`,
+  `The Spark`,
+  `Thumper`,
+  `T.I.N.A.`,
+  `Today Won’t Go Down in History`,
+  `Torn Apart`,
+  `Tribalism`,
+  `Undercover Agents`,
+  `Wall`,
+  `Waltzing Off The Face Of The Earth (I. Crescendo)`,
+  `Waltzing off the Face of the Earth (II. Piangevole)`,
+  `Warm Smiles Do Not Make You Welcome Here`,
+  `We Can Breathe In Space`,
+  `When a Jealous Man Finds a Gun`,
+  `Zzzonked`
+]
 
 
 
@@ -229,3 +178,23 @@ function randomInteger(min, max) {
   document.querySelector('.show-answer').addEventListener('click', () => {
     document.querySelector('.title').innerText = `${artist} — ${songTitle}`;
   })
+
+
+
+
+
+  // async function searchSongsTest(song) {
+  //   try {
+  //     const res = await fetch(`${apiURL}/v1/${artist}/${song}`);
+  //     const data = await res.json();
+  //     const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+  //     console.log('Good');
+  //   } catch (error) {
+  //     console.log(error, song);
+  //   }
+  // }
+
+  // tracks.forEach(song => {
+  //   searchSongsTest(song);
+  // });
+
--- songBase.js
+++ songBase.js
@@ -1,163 +1,108 @@
 
-const tracks = [`Acid Nation`,
-
-`Frozen Landscape`,
-
+const tracks = [
+`Acid Nation`,
 `Adieu`,
-`Airfield`, 
-`All Eyes on the Saint`, 
+`Airfield`,
+`All Eyes on the Saint`,
 `Anaesthetist`,
 `An Ode to Lost Jigsaw Pieces (In Two Movements)`,
- `Antwerpen`,
-  `Anything Can Happen in the Next Half Hour…`, `​apøcaholics anonymøus (main theme in B minor)
-`, 
-`Arguing with Thermometers
-`,
- `Common Dreads
-`,
- `Constellations
-`,
- `Crossing The Rubicon
-`, 
-`Dear Future Historians…
-`, 
-`Destabilise
-`,
- `Elegy For Extinction
-`,
- `Empty
-`, 
+`Antwerpen`,
+`Anything Can Happen in the Next Half Hour…`,
+`apøcaholics anonymøus (main theme in B minor)`,
+`Arguing with Thermometers`,
+`Common Dreads`,
+`Constellations`,
+`Crossing The Rubicon`,
+`Dear Future Historians…`,
+`Destabilise`,
+`Elegy For Extinction`,
 `Enter Shikari`,
-`Fanfare for the Conscious Man
-`,
- `Gandhi Mate, Gandhi
-`,
- `Gap in the Fence
-`,
- `Havoc A
-`,
- `Havoc B
-`,
- `Hectic
-`,
- `Hello Tyrannosaurus, Meet Tyrannicide
-`, 
-`Hoodwinker
-`,
- `Interlude
-`, 
-`Jonny Sniper
-`,
- `Juggernauts
-`, 
-`Keep It on Ice
-`,
- `Kickin’ Back on the Surface of Your Cheek
-`,
- `Know Your Enemy
-`,
- `Labyrinth
-`,
- `Live Outside
-`,
- `Marionettes (II. The Ascent)
-`, 
-`Marionettes (I. The Discovery Of Strings)
-`, 
-`…Meltdown
-`, 
-`​modern living....
-`, 
-`Mothership
-`, `Myopia
-`, `Never Let Go of the Microscope
-`, `Nodding Acquaintance
-`, `No Sleep Tonight
-`, `No Sssweat
-`, `OK, Time for Plan B
-`, `Pack of Thieves
-`, `Quelle Surprise
-`, `Rabble Rouser
-`, `Radiate
-`, `Rat Race
-`, `Redshift
-`, `Return to Energiser
-`, `​satellites* *
-`, `Score 22
-`, `Search Party
-`, `Shinrin-yoku
-`, `Slipshod
-`, `Solidarity
-`, `Sorry, You’re Not a Winner
-`, `Sssnakepit
-`, `Stalemate
-`, `Stand Your Ground; This Is Ancient Land
-`, `Step Up
-`, `Stop the Clocks
-`, `Supercharge
-`, `System…
-`, `Take My Country Back
-`, `The Appeal & the Mindsweep I
-`, `The Appeal & the Mindsweep II
-`, `The Bank of England
-`, `The Bearer of Bad News`,
-
-
-`{ The Dreamer’s Hotel }
-`, 
-`The Feast
-`, 
-`THE GREAT UNKNOWN
-`, 
-`The Jester
-`, 
-`​thē kĭñg
-`, 
-`The Last Garrison
-`, 
-`The One True Colour
-`, 
-`The Paddington Frisk
-`, 
-`​the pressure’s on.
-`, 
-`There’s a Price on Your Head
-`, 
-`The Revolt of the Atoms
-`, 
-`The Sights
-`, 
-`The Spark
-`, 
-`Thumper
-`, 
-`T.I.N.A.
-`, 
-`Today Won’t Go Down in History
-`, 
-`Torn Apart
-`, 
-`Tribalism
-`, 
-`Undercover Agents
-`, 
-`Wall
-`, 
-`Waltzing Off The Face Of The Earth (I. Crescendo)
-`, 
-`Waltzing off the Face of the Earth (II. Piangevole)
-`, 
-`Warm Smiles Do Not Make You Welcome Here
-`, 
-`We Can Breathe In Space
-`, 
-`When a Jealous Man Finds a Gun
-`, 
+`Empty`,
+`Fanfare for the Conscious Man`,
+`Fixed Eyes`,
+`Frozen Landscape`,
+`Gandhi Mate, Gandhi`,
+`Gap in the Fence`,
+`Havoc A`,
+`Havoc B`,
+`Hectic`,
+`Hello Tyrannosaurus, Meet Tyrannicide`,
+`Hoodwinker`,
+`Insomnia`,
+`Interlude`,
+`Jonny Sniper`,
+`Juggernauts`,
+`Keep It on Ice`,
+`Kickin’ Back on the Surface of Your Cheek`,
+`Know Your Enemy`,
+`Labyrinth`,
+`Live Outside`,
+`Marionettes (II. The Ascent)`,
+`Marionettes (I. The Discovery Of Strings)`,
+`…Meltdown`,
+`modern living....`,
+`Mothership`,
+`Myopia`,
+`Never Let Go of the Microscope`,
+`Nodding Acquaintance`,
+`No Sleep Tonight`,
+`No Sssweat`,
+`OK, Time for Plan B`,
+`Pack of Thieves`,
+`Quelle Surprise`,
+`Rabble Rouser`,
+`Radiate`,
+`Rat Race`,
+`Redshift`,
+`Return to Energiser`,
+`satellites* *`,
+`Score 22`,
+`Search Party`,
+`Shinrin-yoku`,
+`Slipshod`,
+`Solidarity`,
+`Sorry, You’re Not a Winner`,
+`Sssnakepit`,
+`Stalemate`,
+`Stand Your Ground; This Is Ancient Land`,
+`Step Up`,
+`Stop the Clocks`,
+`Supercharge`,
+`System…`,
+`Take My Country Back`,
+`The Appeal & the Mindsweep I`,
+`The Appeal & the Mindsweep II`,
+`The Bank of England`,
+`The Bearer of Bad News`,
+`{ The Dreamer’s Hotel }`,
+`The Feast`,
+`THE GREAT UNKNOWN`,
+`The Jester`,
+`thē kĭñg`,
+`The Last Garrison`,
+`The One True Colour`,
+`The Paddington Frisk`,
+`the pressure’s on.`,
+`There’s a Price on Your Head`,
+`The Revolt of the Atoms`,
+`The Sights`,
+`The Spark`,
+`Thumper`,
+`T.I.N.A.`,
+`Today Won’t Go Down in History`,
+`Torn Apart`,
+`Tribalism`,
+`Undercover Agents`,
+`Wall`,
+`Waltzing Off The Face Of The Earth (I. Crescendo)`,
+`Waltzing off the Face of the Earth (II. Piangevole)`,
+`Warm Smiles Do Not Make You Welcome Here`,
+`We Can Breathe In Space`,
+`When a Jealous Man Finds a Gun`,
 `Zzzonked`
 ]
 
-module.exports = tracks;
+// module.exports = tracks;
+module.exports = { tracks }
 
-const res = await fetch(`https://api.lyrics.ovh/v1/Enter Shikari/Frozen Landscape`);
-const data = await res.json();
-console.log(data)
+
